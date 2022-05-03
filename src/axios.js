778b8b7c1831df66308281c44611f512dbb93978
Alfred Githinji <alfredgithinji87@gmail.com>
2022-05-03 13:36:05 +0300

    modified baseUrl link

--- src/axios.js
+++ src/axios.js
@@ -2,6 +2,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:3003/",
+  // baseURL: "http://localhost:3003/",
+  baseURL: "https://freds-portfolio-backend.herokuapp.com/",
 });
 export default instance;
