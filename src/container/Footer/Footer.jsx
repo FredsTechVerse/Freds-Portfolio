30432a6a0a9fd12412c32255625e74a849f7e47c
Alfred Githinji <alfredgithinji87@gmail.com>
2022-05-08 02:33:13 +0300

    Modified contact info

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -45,7 +45,7 @@ const Footer = () => {
       <div className="app__footer-cards">
         <div className="app__footer-card ">
           <img src={images.email} alt="email" />
-          <a href="mailto:hello@micael.com" className="p-text">
+          <a href="mailto:alfredgithinji87@gmail.com" className="p-text">
             alfredgithinji87@gmail.com
           </a>
         </div>
