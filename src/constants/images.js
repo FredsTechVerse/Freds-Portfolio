82e4dd147886eebbd654f121b425dead793a5a64
Alfred Githinji <alfredgithinji87@gmail.com>
2022-06-24 22:38:54 +0300

    Changed footer heading

--- src/constants/images.js
+++ src/constants/images.js
@@ -25,8 +25,8 @@ import about01 from "../assets/about01.png";
 import about02 from "../assets/about02.png";
 import about03 from "../assets/about03.png";
 import about04 from "../assets/about04.png";
-import profile from "../assets/profile.png";
 import profile2 from "../assets/profile2.png";
+import portfolio from "../assets/portfolio.png";
 import mongoDB from "../assets/mongoDB.png";
 import bootstrap from "../assets/bootstrap.png";
 import circle from "../assets/circle.svg";
@@ -48,6 +48,7 @@ const images = {
   css,
   express,
   figma,
+  portfolio,
   flutter,
   git,
   mongoDB,
@@ -71,7 +72,6 @@ const images = {
   about02,
   about03,
   about04,
-  profile,
   circle,
   logo,
   adidas,
